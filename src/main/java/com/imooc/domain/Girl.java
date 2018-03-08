c8d5871d0d0f3566ae41ddd268bc656f0377db1b
wangchi <f2114198@163.com>
2018-03-08 11:42:41 +0800

    使用注解对传入字段进行验证

--- src/main/java/com/imooc/domain/Girl.java
+++ src/main/java/com/imooc/domain/Girl.java
@@ -3,6 +3,7 @@ package com.imooc.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 
 /**
  * Created by Jackie on 2018/3/7.
@@ -16,6 +17,7 @@ public class Girl {
     @GeneratedValue // 自增序列
     private Integer id;
     private String cupSize;
+    @Min(value = 18, message = "未成年少女禁止入内")
     private Integer age;
 
     public Integer getId() {
